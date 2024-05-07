1f8fd5b27e3121e55e3408b28890e5053f4aaf29
John Pywtorak <jpywtora@gmail.com>
2024-05-06 22:18:48 -0700

    Updated outline and edgy

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,8 +1,9 @@
 return {
   "hedyhli/outline.nvim",
-  -- dependencies = {
-  --   "folke/edgy.nvim",
-  -- },
+  dependencies = {
+    -- "The `edgy.nvim` must be **imported** before the `outline.nvim` extra to work properly.",
+    "folke/edgy.nvim",
+  },
   keys = { { "<leader>cO", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
   cmd = "Outline",
   opts = function()
@@ -15,13 +16,13 @@ return {
     local filter = Config.kind_filter
 
     if type(filter) == "table" then
-      filter = filter.default
-      if type(filter) == "table" then
+      if type(filter.default) == "table" then
         for kind, symbol in pairs(defaults.symbols) do
           opts.symbols[kind] = {
             icon = Config.icons.kinds[kind] or symbol.icon,
             hl = symbol.hl,
           }
+
           if not vim.tbl_contains(filter, kind) then
             table.insert(opts.symbol_blacklist, kind)
           end
